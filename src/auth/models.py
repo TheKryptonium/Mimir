8b24073da026c3be6edce9426bfcba6b400c9618
Kryptonium <lekryptonium@gmail.com>
2026-03-05 21:56:54 +0100

    User auth model

---
+++ src/auth/models.py
@@ -0,0 +1,26 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime
+import uuid
+
+class User(SQLModel, table= True):
+    __tablename__="users"
+    
+    uid: uuid.UUID = Field(
+        default_factory = uuid.uuid4,
+        sa_column = Column(
+            pg.UUID,
+            nullable = False,
+            primary_key = True,
+        )
+    )
+    username: str
+    email: str
+    firstname: str
+    lastname: str
+    is_verified: bool = False
+    created_at: datetime = Field(sa_column = Column(pg.TIMESTAMP(timezone=True), default=datetime.now))
+    updated_at: datetime = Field(sa_column = Column(pg.TIMESTAMP(timezone=True), default=datetime.now))
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
