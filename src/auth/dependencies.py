7cdf0efdafa5143dffbc036fc237d2c7f86e7287
Kryptonium <lekryptonium@gmail.com>
2026-05-14 17:58:05 +0100

    HTTP Bearer Authentication

---
+++ src/auth/dependencies.py
@@ -0,0 +1,26 @@
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import Request, HTTPException, status
+from .utils import decode_token
+
+class AccessTokenBearer(HTTPBearer):
+    
+    def __init__(self, auto_error=True):
+        super().__init__(auto_error=auto_error)
+        
+    async def __call__(self, request: Request)->HTTPAuthorizationCredentials|None:
+        creds = await super().__call__(request)
+        
+        token = creds.credentials
+        
+        if not self.validate_token(token):
+            raise HTTPException(
+                status_code = status.HTTP_403_FORBIDDEN, 
+                detail="Provide an access token"
+                )
+        
+        return creds
+    
+    def validate_token(self, token: str)-> bool:
+        token_data = decode_token(token)
+        
+        return True if token_data is not None else False
--- src/books/routes.py
+++ src/books/routes.py
@@ -4,9 +4,11 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from src.db.main import get_session
 from .service import BookService
 from src.books.schemas import Book, BookUpdateModel, BookCreateModel
+from src.auth.dependencies import AccessTokenBearer
 
 book_router = APIRouter()
 book_service = BookService()
+access_token_bearer = AccessTokenBearer()
 
 @book_router.get('/', response_model = List[Book])
 async def get_all_books(session: AsyncSession = Depends(get_session))-> List[Book]:
@@ -23,12 +25,16 @@ async def get_book(book_id: str, session: AsyncSession=Depends(get_session))->di
     
 
 @book_router.post('/', status_code = status.HTTP_201_CREATED, response_model = Book)
-async def create_book(book: BookCreateModel, session: AsyncSession=Depends(get_session))->Book:
+async def create_book(book: BookCreateModel, 
+                    session: AsyncSession=Depends(get_session),
+                    user_details = Depends(access_token_bearer))->Book:
     new_book = await book_service.create_book(session, book)
     return new_book
 
 @book_router.patch('/{book_id}')
-async def update_book(book_id: str, book_updates: BookUpdateModel, session: AsyncSession=Depends(get_session)):
+async def update_book(book_id: str, book_updates: BookUpdateModel, 
+                    session: AsyncSession=Depends(get_session),
+                    user_details = Depends(access_token_bearer)):
     update_book = await book_service.update_book(session, book_id, book_updates)
     if update_book:
         return update_book
@@ -37,7 +43,9 @@ async def update_book(book_id: str, book_updates: BookUpdateModel, session: Asyn
 
 
 @book_router.delete('/{book_id}', status_code = status.HTTP_204_NO_CONTENT)
-async def delete_book(book_id: str, session: AsyncSession = Depends(get_session)):
+async def delete_book(book_id: str, 
+                    session: AsyncSession = Depends(get_session),
+                    user_details = Depends(access_token_bearer)):
     book_to_delete = await book_service.delete_book(session, book_id)
 
     if book_to_delete:
